2dfee5df9359febc01ab82bd10e2e63d7ddb8924
Sidi Mohammed Ouaret <souaret@student.42paris.fr>
2024-05-31 20:04:43 +0200

    Libft - issue fixed : ft_lstmap.c (paco --strict) OK: 15

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,34 +6,45 @@
 /*   By: souaret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 21:28:09 by souaret           #+#    #+#             */
-/*   Updated: 2024/05/29 17:36:01 by souaret          ###   ########.fr       */
+/*   Updated: 2024/05/31 17:09:29 by souaret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+35			ft_lstclear(&first, del);
+
+43			// del(new_content);
+44			// ft_lstclear(&first, del);
+45			// return (NULL);
+*/
+static t_list	*ft_clear_up(t_list *first, void *new_c, void (*del)(void *))
+{
+	del(new_c);
+	ft_lstclear(&first, del);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*first;
 	t_list	*new;
+	void	*new_c;
 
 	if (!f || !del)
 		return (NULL);
 	first = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
-		if (!(new))
+		new_c = (*f)(lst->content);
+		if (!new_c)
 		{
-			while (first)
-			{
-				new = first->next;
-				(*del)(first->content);
-				free(first);
-				first = new;
-			}
-			lst = NULL;
-			return (NULL);
+			return (ft_clear_up(first, new_c, del));
+		}
+		new = ft_lstnew(new_c);
+		if (new == NULL)
+		{
+			return (ft_clear_up(first, new_c, del));
 		}
 		ft_lstadd_back(&first, new);
 		lst = lst->next;
